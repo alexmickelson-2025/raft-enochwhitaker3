a23a6f16c270d03121ccc162a69e1ae199475249
Enoch Whitaker <enochwhitaker3@gmail.com>
2025-02-03 11:56:11 -0700

    Added docker client

---
+++ RaftTests/RaftLibrary/NodeData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaftLibrary;
+
+public record NodeData
+{
+    int Id;
+}
